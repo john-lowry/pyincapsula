57075fd03372eda9894dc4905d7030b443c6885a
j-lowry <johnlowry@gmail.com>
2017-07-26 11:21:59 -0700

    Add modSiteACL
    Right now it just allows for urls and country blocking, need to get url
    patterns and continents

--- incapsula/incapsula.py
+++ incapsula/incapsula.py
@@ -31,9 +31,18 @@ def modSiteLogLevel
 def modSiteSecurityConfig
 '''
 
-'''
-def modSiteACL
-'''
+def modSiteACL(site_id, api_id=os.environ.get('API_ID'), api_key=os.environ.get('API_KEY'), rule_id, *urls, *url_patterns, *countries, *continents, *ips):
+    if rule_id == 'api.acl.blacklisted_countries':
+        payload={'api_id': api_id,'api_key': api_key, 'site_id': site_id, 'rule_id': rule_id, 'countries': countries}
+    if rule_id == 'api.acl.blacklisted_urls':
+        payload={'api_id': api_id,'api_key': api_key, 'site_id': site_id, 'rule_id': rule_id, 'urls': urls}
+    if rule_id == 'api.acl.blacklisted_ips':
+        payload={'api_id': api_id,'api_key': api_key, 'site_id': site_id, 'rule_id': rule_id, 'ips': ips}
+    if rule_id == 'api.acl.whitelisted_ips':
+        payload={'api_id': api_id,'api_key': api_key, 'site_id': site_id, 'rule_id': rule_id, 'ips': ips}
+    url=api_endpoint + 'prov/v1/sites/configure/acl'
+    r = requests.post(url, data=payload)
+    return r.text
 
 '''
 def modWhitelistConfig

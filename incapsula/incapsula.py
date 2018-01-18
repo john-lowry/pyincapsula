0c0554ae92d39620dc5436577a32bb8b4178090d
John Lowry <john.lowry@cambiahealth.com>
2018-01-18 00:22:03 +0000

    Add the ability to modify loglevel

--- incapsula/incapsula.py
+++ incapsula/incapsula.py
@@ -23,9 +23,12 @@ def getDomainApproverEmail
 def modSiteConfig
 '''
 
-'''
-def modSiteLogLevel
-'''
+
+def modSiteLogLevel(site_id, log_level, api_id=os.environ.get('API_ID'), api_key=os.environ.get('API_KEY')):
+    url=api_endpoint + 'prov/v1/sites/setlog'
+    payload={'api_id': api_id,'api_key': api_key, 'site_id': site_id, 'log_level': log_level}
+    r = requests.post(url, data=payload)
+    return r.text
 
 '''
 def modSiteSecurityConfig

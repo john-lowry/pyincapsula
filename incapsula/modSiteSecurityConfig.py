2f8eda4e87e5c608c23222c4b9e1b6273f3c544d
Steven Joyal <Steven.Joyal@cambiahealth.com>
2019-02-13 13:31:06 -0800

    Finished security settings, incremented version
    updated __init__.py to python3 absolute import
    
    Added in remainder of security settings
    
    incremented version

--- incapsula/modSiteSecurityConfig.py
+++ incapsula/modSiteSecurityConfig.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python
+#!python3
+
+# Changes security settings for the sepesified site
+# Requires Site ID, the rule id, and, the value to assign
+# Values are either a number (DDoS) True/False (Bot Blocking) or the action 
+# without the api.threats.action portion, as that is appended
+# 
+# For Bot blocking we assume that we should challange suspected bot unless told otherwise
+# Valid values are listed at: https://docs.incapsula.com/Content/API/sites-api.htm#Modify3
+# This is only a partial implementation, more still needs to be added
 
 import os
 import requests
@@ -13,7 +22,7 @@ def modSiteSecurityConfig(site_id,rule_id,value,api_id=os.environ.get('API_ID'),
             'api_key':api_key,
             'site_id':site_id,
             'rule_id':rule_id,
-            'activation_mode':'api.threats.ddos.activiation_mode.auto', #This is required, can also be on
+            'activation_mode':'api.threats.ddos.activation_mode.auto', #This is required, can also be on
             'ddos_traffic_threshold':value
         }
     elif rule_id=='api.threats.bot_access_control':
@@ -25,8 +34,20 @@ def modSiteSecurityConfig(site_id,rule_id,value,api_id=os.environ.get('API_ID'),
             'block_bad_bots':value,
             'challenge_suspected_bots':chal_sus_bot
         }
+    elif rule_id=='api.threats.remote_file_inclusion'\
+        or rule_id=='api.threats.sql_injection'\
+        or rule_id=='api.threats.cross_site_scripting'\
+        or rule_id=='api.threats.illegal_resource_access'\
+        or rule_id=='api.threats.backdoor':
+        value='api.threats.action.'+value
+        payload={
+            'api_id':api_id,
+            'api_key':api_key,
+            'site_id':site_id,
+            'rule_id':rule_id,
+            'security_rule_action':value
+        }
     else:
         return 1
     r=requests.post(url, data=payload)
     return r.text
-

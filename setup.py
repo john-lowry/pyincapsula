b73de3c01255977310aee59028da103dd7eafe5f
john-lowry <johnlowry@gmail.com>
2019-04-09 08:02:33 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='incapsula', 
-    version='0.7.3.0',
+    version='0.7.4',
     author='John Lowry',
     author_email='johnlowry@gmail.com',
     description='A python module for using the Incapsula API',
